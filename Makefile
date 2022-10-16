aaa20c443ed2ef6ae8ed1fa8d8543c94c903400d
wojnet <wojnetto@gmail.com>
2022-10-17 01:55:35 +0200

    Idzie calkiem okej

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all:
-	mingw32-g++ -I src/include -L src/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2
+	mingw32-g++ -I src/include -L src/lib -o main *.cpp -lmingw32 -lSDL2main -lSDL2
 	./main
--- main.cpp
+++ main.cpp
@@ -3,27 +3,48 @@
 
 const int WIDTH = 800, HEIGHT = 600;
 
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Event windowEvent;
+
+bool isRunning = true;
+
+void input() {
+    if (SDL_PollEvent(&windowEvent)) {
+        if (SDL_QUIT == windowEvent.type) {
+            isRunning = false;
+        }
+    }
+}
+
+void update() {
+    
+}
+
+void draw() {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+
+
 int main(int argc, char *argv[]) {
 
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-    if (nullptr == window) {
-        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
-    }
+    window = SDL_CreateWindow("TopDownEngine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    if (nullptr == window) std::cout << "Could not create window: " << SDL_GetError() << std::endl;
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (nullptr == renderer) {
-        std::cout << "Could not create renderer: " << SDL_GetError() << std::endl;
-    }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (nullptr == renderer) std::cout << "Could not create renderer: " << SDL_GetError() << std::endl;
 
-    SDL_Event windowEvent;
-    while(true) {
-        if (SDL_PollEvent(&windowEvent)) {
-            if (SDL_QUIT == windowEvent.type) {
-                break;
-            }
-        }
+    while (isRunning) {
+
+        input();
+        update();
+        draw();
+
     }
 
     SDL_DestroyWindow(window);
