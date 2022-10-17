8f420f909c8430131029f0e16222bfe732858c2a
wojnet <wojnetto@gmail.com>
2022-10-17 10:06:51 +0200

    Okej

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
-#include "Player.cpp"
+// #include "Game.cpp"
 
 const int WIDTH = 800, HEIGHT = 600;
 
@@ -8,45 +8,37 @@ SDL_Window *window;
 SDL_Renderer *renderer;
 SDL_Event windowEvent;
 
-Player player(64, 64, 64, 64);
-
 bool isRunning = true;
 
-void input() {
-    if (SDL_PollEvent(&windowEvent)) {
-        if (SDL_QUIT == windowEvent.type) {
-            isRunning = false;
-        }
-    }
-}
+// void input() {
+//     if (SDL_PollEvent(&windowEvent)) {
+//         if (SDL_QUIT == windowEvent.type) {
+//             isRunning = false;
+//         }
+//     }
+// }
 
-void update() {
+// void update() {
     
-}
+// }
 
-void draw() {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-}
+// void draw() {
+//     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+//     SDL_RenderClear(renderer);
+//     SDL_RenderPresent(renderer);
+// }
 
-
+game* = new Game();
 
 int main(int argc, char *argv[]) {
 
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    window = SDL_CreateWindow("TopDownEngine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-    if (nullptr == window) std::cout << "Could not create window: " << SDL_GetError() << std::endl;
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (nullptr == renderer) std::cout << "Could not create renderer: " << SDL_GetError() << std::endl;
+    game->init("Game", 0, 0, 800, 600, 0);
 
     while (isRunning) {
 
-        input();
-        update();
-        draw();
+        // input();
+        // update();
+        // draw();
 
     }
 
